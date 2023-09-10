39548e4455ebca6021e5121c2c5c40c09fb6f8a9
Anton Makarov <ontko@mailfence.com>
2023-09-10 15:05:20 +0300

    Rewrote routing without recursion

--- lib/router.rb
+++ lib/router.rb
@@ -12,12 +12,19 @@ class Router
   end
 
   def handle_request(req)
-    action = match_action(req.path, req.method, @routes)
+    action = match_action(req.path, req.method)
     action.call(req)
   end
 
-  def define_route(method, path, routes = @routes, &action)
-    def_route(method, path.sub(%r{^/*(.*?)/*$}, '\1'), routes, &action)
+  def define_route(method, path, &action)
+    current_route = @routes
+    path.sub(%r{^/*(.*?)/*$}, '\1').split('/').each do |subpath|
+      current_route[subpath] = {} unless current_route[subpath]
+      current_route = current_route[subpath]
+    end
+
+    current_route[''] = {} unless current_route['']
+    current_route[''][method] = action
   end
 
   def params_for_path(path)
@@ -41,36 +48,27 @@ class Router
 
   private
 
-  def def_route(method, path, routes, &action)
-    if path.empty?
-      return routes[path][method] = action if routes[path]
+  def match_action(path, method)
+    current_route = @routes
+    path.sub(%r{^/*(.*?)/*$}, '\1').split('/').each do |subpath|
+      if current_route[subpath].nil?
+        key = current_route.keys.find { |k| k.start_with?(':') }
 
-      return routes[path] = { method => action }
+        raise NotFound unless key
+
+        current_route = current_route[key]
+        next
+      end
+
+      current_route = current_route[subpath]
     end
 
-    current_path, next_path = path.split('/', 2)
-    next_path ||= ''
-    routes[current_path] = {} unless routes[current_path]
-    next_routes = routes[current_path]
+    methods = current_route['']
+    raise NotFound unless methods
 
-    def_route(method, next_path, next_routes, &action)
-  end
+    action = methods[method]
+    raise MethodNotAllowed unless action
 
-  def match_action(path, method, routes)
-    if path.empty?
-      raise NotFound unless routes && routes[path]
-
-      action = routes[path][method]
-      raise MethodNotAllowed unless action
-
-      return action
-    end
-
-    split_path = path.split('/', 2)
-    current_path = split_path[0]
-    next_path = split_path[1] || ''
-    next_routes = routes[current_path] || routes[routes.keys.find { |k| k.start_with?(':') }]
-
-    match_action(next_path, method, next_routes)
+    action
   end
 end
